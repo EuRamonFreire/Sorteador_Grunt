172b4085f8072baa614aa1b3b3ed8f6b67da3435
Ramon Freire <ramonfreirealves@outlook.com>
2024-09-20 17:47:36 -0300

    intial commit

---
+++ gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css' : 'main.less'
+                }
+            }
+        }
+    })
+
+    grunt.registerTask('less', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log('olá Grunt');
+            done();
+        }, 3000);
+    })
+
+    grunt.loadNpmTasks('grunt-contib-less');
+    grunt.registerTask('default', ['less']);
+
+}
